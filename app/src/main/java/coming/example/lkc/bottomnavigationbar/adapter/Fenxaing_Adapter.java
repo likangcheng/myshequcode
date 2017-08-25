418efa5db60898132461b67f1585826e65a45915
likangcheng <likangcheng324@gmail.com>
2017-08-25 16:26:18 +0800

    添加微信分享功能

---
+++ app/src/main/java/coming/example/lkc/bottomnavigationbar/adapter/Fenxaing_Adapter.java
@@ -0,0 +1,7 @@
+package coming.example.lkc.bottomnavigationbar.adapter;
+
+/**
+ * Created by lkc on 2017/8/25.
+ */
+public class Fenxaing_Adapter {
+}
---
+++ app/src/main/java/coming/example/lkc/bottomnavigationbar/wxapi/WXEntryActivity.java
@@ -0,0 +1,13 @@
+package coming.example.lkc.bottomnavigationbar.wxapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WXEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wxentry);
+    }
+}
